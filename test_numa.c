10688992c2f7688d6b8c7545a184dab040ede332
David Sanftenberg <david.sanftenberg@gmail.com>
2025-09-12 19:27:15 +0000

    remove test files

--- test_numa.c
+++
@@ -1,8 +0,0 @@
-#include <numa.h>
-#include <stdio.h>
-int main() {
-    printf("NUMA available: %s\n", numa_available() ? "true" : "false");
-    printf("NUMA nodes: %d\n", numa_num_configured_nodes());
-    printf("NUMA max nodes: %d\n", numa_max_node() + 1);
-    return 0;
-}
